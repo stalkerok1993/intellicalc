fd2d40f2fb3a67d10c3374cf2ae4d4729ec4785d
Serhii Bigun <stalkerok1993@gmail.com>
2015-08-16 12:13:13 +0300

    fixed all operation classes in packege 'basic'

--- app/src/main/java/ua/org/s4code/intellicalc/analyser/function/basic/Factorial.java
+++ app/src/main/java/ua/org/s4code/intellicalc/analyser/function/basic/Factorial.java
@@ -1,10 +1,13 @@
 package ua.org.s4code.intellicalc.analyser.function.basic;
 
 import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
 import ua.org.s4code.intellicalc.analyser.ExprContainer;
 import ua.org.s4code.intellicalc.analyser.Expression;
 import ua.org.s4code.intellicalc.analyser.function.Function;
+import ua.org.s4code.intellicalc.analyser.value.Literal;
+import ua.org.s4code.intellicalc.analyser.value.ValueType;
 
 /**
  * Created by Serhii on 8/11/2015.
@@ -12,10 +15,29 @@ import ua.org.s4code.intellicalc.analyser.function.Function;
 public class Factorial extends Function {
     @Override
     public Expression count(ExprContainer context, ArrayList<Expression> arguments) throws Exception {
-        return null;
+        Expression result = null;
+
+        switch (arguments.size()) {
+            case 0:
+                throw new Exception("There are lack of operands.");
+            case 1:
+                if (Function.isValues(arguments)) {
+                    double acc = ((ValueType) arguments.get(0)).getValue(context);
+                    acc = fct(((long) acc));
+
+                    result = new Literal(acc);
+                } else {
+                    throw new Exception("Type of arguments is not permitted.");
+                }
+                break;
+            default:
+                throw new Exception("Too many operands.");
+        }
+
+        return result;
     }
 
-    public double fct(long op) {
+    private static double fct(long op) {
         if (op < 0) {
             throw new IllegalArgumentException("op MUST be a positive integer or zero.");
         }
--- app/src/main/java/ua/org/s4code/intellicalc/analyser/function/basic/Mul.java
+++ app/src/main/java/ua/org/s4code/intellicalc/analyser/function/basic/Mul.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import ua.org.s4code.intellicalc.analyser.ExprContainer;
 import ua.org.s4code.intellicalc.analyser.Expression;
 import ua.org.s4code.intellicalc.analyser.function.Function;
+import ua.org.s4code.intellicalc.analyser.value.Literal;
+import ua.org.s4code.intellicalc.analyser.value.ValueType;
 
 /**
  * Created by Serhii on 8/11/2015.
@@ -12,6 +14,26 @@ import ua.org.s4code.intellicalc.analyser.function.Function;
 public class Mul extends Function {
     @Override
     public Expression count(ExprContainer context, ArrayList<Expression> arguments) throws Exception {
-        return null;
+        Expression result = null;
+
+        switch (arguments.size()) {
+            case 0:
+                // fall through
+            case 1:
+                throw new Exception("There are lack of operands.");
+            default:
+                if (Function.isValues(arguments)) {
+                    double acc = ((ValueType) arguments.get(0)).getValue(context);
+                    for (int i = 1; i < arguments.size(); i++) {
+                        acc *= ((ValueType) arguments.get(i)).getValue(context);
+                    }
+
+                    result = new Literal(acc);
+                } else {
+                    throw new Exception("Type of arguments is not permitted.");
+                }
+        }
+
+        return result;
     }
 }
